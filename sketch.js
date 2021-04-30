5eee4ee588d5c92df53c8f82fe4b9ad13918ea20
ankitghagre <78957478+ankitghagre@users.noreply.github.com>
2021-04-30 15:42:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+var canvas,backgroundImage,playerCount,database,form,player,game;
+var gameState = 0;
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  
+}
+function draw(){
+  
+}
